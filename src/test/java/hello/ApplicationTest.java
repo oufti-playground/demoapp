9c2fb637bf6b873ef824ea64ea5d161f2753089e
Damien Duportal <damien.duportal@gmail.com>
2017-04-25 09:39:14 +0200

    Update ApplicationTest.java

--- src/test/java/hello/ApplicationTest.java
+++ src/test/java/hello/ApplicationTest.java
@@ -28,6 +28,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
