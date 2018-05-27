39fc5abb8ca71cbcbd8fe162afb2554274c7d180
Damien DUPORTAL <damien.duportal@gmail.com>
2018-05-27 18:40:43 +0200

    Failing Integration Tests
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- src/test/java/hello/HelloControllerIT.java
+++ src/test/java/hello/HelloControllerIT.java
@@ -36,7 +36,7 @@ public class HelloControllerIT {
   public void getHello() throws Exception {
     ResponseEntity<String> response = template.getForEntity(base.toString(),
     String.class);
-    //assertThat(response.getBody(), equalTo("Greetings from Failing Test!"));
-    assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+    assertThat(response.getBody(), equalTo("Greetings from Failing Test!"));
+    // assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
   }
 }
